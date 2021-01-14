718ef3f01a3ffaa7cd421f965e8979a179aac646
Lilian Hunt <lilian.hunt803@gmail.com>
2021-01-14 04:54:16 +0000

    run validator and display json output using http response

--- webapp/src/main/java/com/google/sps/servlets/UploadServlet.java
+++ webapp/src/main/java/com/google/sps/servlets/UploadServlet.java
@@ -15,6 +15,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,7 +27,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
+import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
+import org.mobilitydata.gtfsvalidator.input.GtfsInput;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.table.GtfsFeedContainer;
+import org.mobilitydata.gtfsvalidator.table.GtfsFeedLoader;
+import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
 
 // TO DO: unit tests
 @WebServlet("/fileupload")
@@ -40,7 +48,6 @@ public class UploadServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
     // Ensure the request contains a file
     if (!ServletFileUpload.isMultipartContent(request)) {
       response.getWriter().println("Error: Form must has enctype=multipart/form-data.");
@@ -76,13 +83,56 @@ public class UploadServlet extends HttpServlet {
             // Save the file on disk
             item.write(storeFile);
 
-            // TO DO: Call validator here 
+            // TO DO: get feed name and thread specification from form
+            String validatorOutputJson = runValidator(filePath, 1, "nl-openov");
+
             response.getWriter().println("Upload has been done successfully!");
+            // Print the json output 
+            response.getWriter().println(validatorOutputJson);
           }
         }
       }
     } catch (Exception ex) {
       response.getWriter().println("There was an error: " + ex.getMessage());
     }
+  }
+
+  // Calls the global mobility validator function to load and validate the transit data
+  private String runValidator(String filePath, int numThreads, String feedNameString) {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    GtfsFeedLoader feedLoader = new GtfsFeedLoader();
+
+    GtfsFeedName feedName = GtfsFeedName.parseString(feedNameString);
+    final long startNanos = System.nanoTime();
+
+    // Input.
+    feedLoader.setNumThreads(numThreads);
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsFeedContainer feedContainer;
+    GtfsInput gtfsInput;
+
+    try {
+      gtfsInput = GtfsInput.createFromPath(Paths.get(filePath));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+    feedContainer =
+        feedLoader.loadAndValidate(gtfsInput, feedName, validatorLoader, noticeContainer);
+
+    // Write the output to the temporary file: what happens when multiple files are written?
+    // Need a unique name ... or just write it to reponse and display it as HTML?
+    try {
+      Files.write(Paths.get("/tmp/", "report.json"),
+          noticeContainer.exportJson().getBytes(StandardCharsets.UTF_8));
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
+
+    final long endNanos = System.nanoTime();
+    System.out.println(
+        String.format("Validation took %.3f seconds", (endNanos - startNanos) / 1e9));
+    System.out.println("Table totals: " + feedContainer.tableTotals());
+    return noticeContainer.exportJson();
   }
 }
