603abd740ab520a1bc50127151fd5af4c5241c67
Lilian Hunt <lilian.hunt803@gmail.com>
2021-01-18 05:10:11 +0000

    modify test so they are not harmful change detector tests

--- webapp/src/test/java/com/google/sps/servlets/RunValidatorTest.java
+++ webapp/src/test/java/com/google/sps/servlets/RunValidatorTest.java
@@ -19,23 +19,23 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 
+import static com.google.common.truth.Truth.assertThat;
+
 @RunWith(JUnit4.class)
 public class RunValidatorTest {
   @Test
   public void testSimpleGtfsFeed() {
     NoticeContainer json =
         UploadServlet.runValidator("src/test/resources/SAMPLE.zip", "au-sydney-buses");
-    // String expected =
-    // "{\"notices\":[{\"code\":\"unknown_column\",\"totalNotices\":1,\"notices\":[{\"filename\":\"stop_times.txt\",\"fieldName\":\"drop_off_time\",\"index\":8}]},{\"code\":\"invalid_row_length\",\"totalNotices\":13,\"notices\":[{\"filename\":\"stop_times.txt\",\"csvRowNumber\":17,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":18,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":19,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":20,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":21,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":22,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":23,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":24,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":25,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":26,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":27,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":28,\"rowLength\":5,\"headerCount\":9},{\"filename\":\"stop_times.txt\",\"csvRowNumber\":29,\"rowLength\":5,\"headerCount\":9}]}]}";
-    // Assert.assertEquals(json, expected);
+    assertThat(json.exportJson()).contains("\"code\":\"invalid_row_length\",\"totalNotices\":13");
+    assertThat(json.exportJson()).contains("\"code\":\"unknown_column\",\"totalNotices\":1");
   }
 
   @Test
   public void testBartGtfsFeed() {
     NoticeContainer json =
         UploadServlet.runValidator("src/test/resources/bart_gtfs.zip", "us-bay-area");
-    // String expected =
-    // "{\"notices\":[{\"code\":\"unknown_column\",\"totalNotices\":5,\"notices\":[{\"filename\":\"transfers.txt\",\"fieldName\":\"from_route_id\",\"index\":5},{\"filename\":\"transfers.txt\",\"fieldName\":\"to_route_id\",\"index\":6},{\"filename\":\"transfers.txt\",\"fieldName\":\"from_trip_id\",\"index\":7},{\"filename\":\"transfers.txt\",\"fieldName\":\"to_trip_id\",\"index\":8},{\"filename\":\"trips.txt\",\"fieldName\":\"trip_load_information\",\"index\":8}]},{\"code\":\"unexpected_file\",\"totalNotices\":5,\"notices\":[{\"filename\":\"calendar_attributes.txt\"},{\"filename\":\"rider_categories.txt\"},{\"filename\":\"fare_rider_categories.txt\"},{\"filename\":\"realtime_routes.txt\"},{\"filename\":\"directions.txt\"}]}]}";
-    // Assert.assertEquals(json, expected);
+    assertThat(json.exportJson()).contains("\"code\":\"unknown_column\",\"totalNotices\":5");
+    assertThat(json.exportJson()).contains("\"code\":\"unexpected_file\",\"totalNotices\":5");
   }
 }
