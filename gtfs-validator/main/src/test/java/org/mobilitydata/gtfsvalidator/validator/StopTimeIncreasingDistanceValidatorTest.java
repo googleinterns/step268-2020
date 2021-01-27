20d3f9a9bb3093414b4b833747a928cbadb1f0ee
Lilian Hunt <lilian.hunt803@gmail.com>
2021-01-27 04:08:39 +0000

    format test

--- gtfs-validator/main/src/test/java/org/mobilitydata/gtfsvalidator/validator/StopTimeIncreasingDistanceValidatorTest.java
+++ gtfs-validator/main/src/test/java/org/mobilitydata/gtfsvalidator/validator/StopTimeIncreasingDistanceValidatorTest.java
@@ -23,95 +23,89 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.DecreasingStopTimeDistanceNotice;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsStopTime;
 import org.mobilitydata.gtfsvalidator.table.GtfsStopTimeTableContainer;
 
-
 @RunWith(JUnit4.class)
 public class StopTimeIncreasingDistanceValidatorTest {
-    private final GtfsStopTime stopTime1 =
-            new GtfsStopTime.Builder()
-                    .setCsvRowNumber(1)
-                    .setTripId("trip1")
-                    .setStopId("1001")
-                    .setStopSequence(1)
-                    .setShapeDistTraveled(1.0)
-                    .build();
-    private final GtfsStopTime stopTime2 =
-            new GtfsStopTime.Builder()
-                    .setCsvRowNumber(2)
-                    .setTripId("trip1")
-                    .setStopId("1002")
-                    .setStopSequence(2)
-                    .setShapeDistTraveled(1.5)
-                    .build();
-    private final GtfsStopTime stopTime3 =
-            new GtfsStopTime.Builder()
-                    .setCsvRowNumber(3)
-                    .setTripId("trip1")
-                    .setStopId("1003")
-                    .setStopSequence(3)
-                    .setShapeDistTraveled(1.4)
-                    .build();
-    private final GtfsStopTime stopTime4 =
-            new GtfsStopTime.Builder()
-                    .setCsvRowNumber(3)
-                    .setTripId("trip1")
-                    .setStopId("1003")
-                    .setStopSequence(3)
-                    .setShapeDistTraveled(1.5)
-                    .build();
+  private final GtfsStopTime stopTime1 = new GtfsStopTime.Builder()
+                                             .setCsvRowNumber(1)
+                                             .setTripId("trip1")
+                                             .setStopId("1001")
+                                             .setStopSequence(1)
+                                             .setShapeDistTraveled(1.0)
+                                             .build();
+  private final GtfsStopTime stopTime2 = new GtfsStopTime.Builder()
+                                             .setCsvRowNumber(2)
+                                             .setTripId("trip1")
+                                             .setStopId("1002")
+                                             .setStopSequence(2)
+                                             .setShapeDistTraveled(1.5)
+                                             .build();
+  private final GtfsStopTime stopTime3 = new GtfsStopTime.Builder()
+                                             .setCsvRowNumber(3)
+                                             .setTripId("trip1")
+                                             .setStopId("1003")
+                                             .setStopSequence(3)
+                                             .setShapeDistTraveled(1.4)
+                                             .build();
+  private final GtfsStopTime stopTime4 = new GtfsStopTime.Builder()
+                                             .setCsvRowNumber(3)
+                                             .setTripId("trip1")
+                                             .setStopId("1003")
+                                             .setStopSequence(3)
+                                             .setShapeDistTraveled(1.5)
+                                             .build();
 
-    @Test
-    public void stopTimeIncreasingDistanceShouldNotGenerateNotice() {
-        final NoticeContainer noticeContainer = new NoticeContainer();
-        StopTimeIncreasingDistanceValidator validator = new StopTimeIncreasingDistanceValidator();
+  @Test
+  public void stopTimeIncreasingDistanceShouldNotGenerateNotice() {
+    final NoticeContainer noticeContainer = new NoticeContainer();
+    StopTimeIncreasingDistanceValidator validator = new StopTimeIncreasingDistanceValidator();
 
-        // Create stopTimeTable:
-        List<GtfsStopTime> stopTimes = new ArrayList<>();
-        stopTimes.add(stopTime1);
-        stopTimes.add(stopTime2);
-        validator.table = GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
+    // Create stopTimeTable:
+    List<GtfsStopTime> stopTimes = new ArrayList<>();
+    stopTimes.add(stopTime1);
+    stopTimes.add(stopTime2);
+    validator.table = GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
 
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getNotices()).isEmpty();
-    }
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getNotices()).isEmpty();
+  }
 
-    @Test
-    public void stopTimeDecreasingDistanceShouldGenerateNotice() {
-        final NoticeContainer noticeContainer = new NoticeContainer();
-        StopTimeIncreasingDistanceValidator validator = new StopTimeIncreasingDistanceValidator();
+  @Test
+  public void stopTimeDecreasingDistanceShouldGenerateNotice() {
+    final NoticeContainer noticeContainer = new NoticeContainer();
+    StopTimeIncreasingDistanceValidator validator = new StopTimeIncreasingDistanceValidator();
 
-        // Create stopTimeTable:
-        List<GtfsStopTime> stopTimes = new ArrayList<>();
-        stopTimes.add(stopTime1);
-        stopTimes.add(stopTime2);
-        stopTimes.add(stopTime3);
-        validator.table = GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
+    // Create stopTimeTable:
+    List<GtfsStopTime> stopTimes = new ArrayList<>();
+    stopTimes.add(stopTime1);
+    stopTimes.add(stopTime2);
+    stopTimes.add(stopTime3);
+    validator.table = GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
 
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getNotices())
-                .containsExactly(
-                        new DecreasingStopTimeDistanceNotice("trip1", 3, 3, 1.4, 2, 2, 1.5));
-    }
-    
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getNotices())
+        .containsExactly(new DecreasingStopTimeDistanceNotice("trip1", 3, 3, 1.4, 2, 2, 1.5));
+  }
 
-    // TO DO: Decide what to do when it doesn't change, currently does not change 
-    @Test
-    public void stopTimeNotChangingDistanceShouldNotGenerateNotice() {
-        final NoticeContainer noticeContainer = new NoticeContainer();
-        StopTimeIncreasingDistanceValidator validator = new StopTimeIncreasingDistanceValidator();
+  // When distance travelled doesn't increase or decrease (stays constant), it does not generate a
+  // notice
+  @Test
+  public void stopTimeNotChangingDistanceShouldNotGenerateNotice() {
+    final NoticeContainer noticeContainer = new NoticeContainer();
+    StopTimeIncreasingDistanceValidator validator = new StopTimeIncreasingDistanceValidator();
 
-        // Create stopTimeTable:
-        List<GtfsStopTime> stopTimes = new ArrayList<>();
-        stopTimes.add(stopTime1);
-        stopTimes.add(stopTime2);
-        stopTimes.add(stopTime4);
-        validator.table = GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
+    // Create stopTimeTable:
+    List<GtfsStopTime> stopTimes = new ArrayList<>();
+    stopTimes.add(stopTime1);
+    stopTimes.add(stopTime2);
+    stopTimes.add(stopTime4);
+    validator.table = GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
 
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getNotices()).isEmpty();
-    }
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getNotices()).isEmpty();
+  }
 }
