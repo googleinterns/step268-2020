16c309af57a76b616ca379977d8def1dc6f53a11
Lilian Hunt <lilian.hunt803@gmail.com>
2021-01-27 23:35:35 +0000

    create trip edge arrival validator file

---
+++ gtfs-validator/main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TripEdgeArrivalDepartureTimeValidator.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator.validator;
+
+import com.google.common.collect.Multimaps;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
+import org.mobilitydata.gtfsvalidator.annotation.Inject;
+import org.mobilitydata.gtfsvalidator.notice.DecreasingShapeDistanceNotice;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.table.GtfsStopTimeTableContainer;
+
+
+import java.util.List;
+
+/**
+ * Validates that trip edges (first and last stops) for a trip have both arrival and deperature stop times, for all trips.
+ *
+ * <p>Generated notice: {@link MissingTripEdgeStopTimeNotice}.
+ */
+@GtfsValidator
+public class TripEdgeArrivalDepartureTimeValidator extends FileValidator {
+  @Inject GtfsStopTimeTableContainer table;
+
+  @Override
+  public void validate(NoticeContainer noticeContainer) {
+    // get all trips
+
+    // iterate through them all 
+
+    // check start / end 
+          
+              noticeContainer.addNotice(
+              new MissingTripEdgeStopTimeNotice(
+                  //add fields here
+                  ));
+        
+      
+    
+  }
+}
