c6e12e08042876de2314537a915fb5777aa76bad
Lilian Hunt <lilian.hunt803@gmail.com>
2021-02-08 02:26:51 +0000

    use auto value to avoid hash collisions

--- gtfs-validator/main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TripHasNoDuplicateStopsValidator.java
+++ gtfs-validator/main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TripHasNoDuplicateStopsValidator.java
@@ -16,11 +16,12 @@
 
 package org.mobilitydata.gtfsvalidator.validator;
 
+import com.google.auto.value.AutoValue;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.Objects;
+import java.util.Set;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
 import org.mobilitydata.gtfsvalidator.annotation.Inject;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
@@ -33,10 +34,27 @@ import org.mobilitydata.gtfsvalidator.table.GtfsTrip;
 import org.mobilitydata.gtfsvalidator.table.GtfsTripTableContainer;
 
 /**
+ * Uses AutoValue to compare duplicate stop information easier.
+ * And avoid hash collisions.
+ */
+@AutoValue
+abstract class DuplicateStopIdentifier {
+  static DuplicateStopIdentifier create(String routeId, String stopId1, String stopId2) {
+    return new AutoValue_DuplicateStopIdentifier(routeId, stopId1, stopId2);
+  }
+
+  abstract String routeId();
+
+  abstract String stopId1();
+
+  abstract String stopId2();
+}
+
+/**
  * Validates that for each trip that consecutive stop times do not have the same stop name.
- * It will only generate one notice per route, so if multiple trips on the same route have 
+ * It will only generate one notice per route, so if multiple trips on the same route have
  * the duplicate there will only be 1 notice.
- * 
+ *
  * [Warning]
  *
  * <p>Generated notice: {@link TripWithDuplicateStopNotice}.
@@ -51,7 +69,7 @@ public class TripHasNoDuplicateStopsValidator extends FileValidator {
 
   @Override
   public void validate(NoticeContainer noticeContainer) {
-    Set<Integer> seenDuplicateStops = new HashSet<>();
+    Set<DuplicateStopIdentifier> seenDuplicateStops = new HashSet<>();
     for (GtfsTrip trip : tripTable.getEntities()) {
       final String tripId = trip.tripId();
       List<GtfsStopTime> stopTimesForTrip = stopTimeTable.byTripId(tripId);
@@ -66,21 +84,24 @@ public class TripHasNoDuplicateStopsValidator extends FileValidator {
         GtfsStop stop = stopTable.byStopId(stopTime.stopId());
         if (prevStopName != null) {
           // Create the key with the route and the stop id's alphabetically
-          int combinedStopKey = -1;
+          DuplicateStopIdentifier duplicateStopIdentifier = null;
           if (prevStopId.compareTo(stop.stopId()) >= 0) {
-            combinedStopKey = Objects.hash(trip.routeId(), prevStopId, stop.stopId());
+            duplicateStopIdentifier =
+                DuplicateStopIdentifier.create(trip.routeId(), prevStopId, stop.stopId());
           } else {
-            combinedStopKey = Objects.hash(trip.routeId(), stop.stopId(), prevStopId);
+            duplicateStopIdentifier =
+                DuplicateStopIdentifier.create(trip.routeId(), stop.stopId(), prevStopId);
           }
+
           if (prevStopName.equals(stop.stopName())) {
             // Only create the notice once for each combination of stops
-            if (!seenDuplicateStops.contains(combinedStopKey)) {
+            if (!seenDuplicateStops.contains(duplicateStopIdentifier)) {
               noticeContainer.addNotice(
                   new TripWithDuplicateStopNotice(stop.stopName(), prevStopId, prevCsvRowNumber,
                       stop.stopId(), stop.csvRowNumber(), trip.routeId(), trip.tripId()));
             }
           }
-          seenDuplicateStops.add(combinedStopKey);
+          seenDuplicateStops.add(duplicateStopIdentifier);
         }
         prevStopName = stop.stopName();
         prevStopId = stop.stopId();
