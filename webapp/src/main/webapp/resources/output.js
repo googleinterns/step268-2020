7636c71a5f742564a884e28ce91ace965cd7e0e1
Lilian Hunt <lilian.hunt803@gmail.com>
2021-02-09 05:39:25 +0000

    create soy template

--- webapp/src/main/webapp/resources/output.js
+++ webapp/src/main/webapp/resources/output.js
@@ -47,3 +47,8 @@ function invalid_row_length(params) {
   const template = goog.soy.renderAsElement(validator.templates.invalidRowLength, params);
   document.getElementById("error").appendChild(template);
 }
+
+function inconsistent_agency_field(params) {
+  const template = goog.soy.renderAsElement(validator.templates.inconsistentAgencyField, params);
+  document.getElementById("error").appendChild(template);
+} 
