f766e321dd157830123f67bf424f6ee44393cb9f
Annie <annieyang0812@gmail.com>
2021-02-12 06:34:10 +0000

    change if statement to compare with const value instead of checking if sum is less than 1

--- gtfs-validator/main/src/main/java/org/mobilitydata/gtfsvalidator/validator/CalendarHasNoActiveDaysValidator.java
+++ gtfs-validator/main/src/main/java/org/mobilitydata/gtfsvalidator/validator/CalendarHasNoActiveDaysValidator.java
@@ -30,6 +30,8 @@ import org.mobilitydata.gtfsvalidator.table.GtfsCalendarTableContainer;
  */
 @GtfsValidator
 public class CalendarHasNoActiveDaysValidator extends FileValidator {
+  private final static int NOT_AVAILBLE = 0;
+
   @Inject GtfsCalendarTableContainer calendarTable;
 
   @Override
@@ -37,11 +39,13 @@ public class CalendarHasNoActiveDaysValidator extends FileValidator {
     // Loop through calendar and add up enum values for each day. If value is less than 1, then no
     // days are active.
     for (GtfsCalendar calendar : calendarTable.getEntities()) {
-      if ((calendar.monday().getNumber() + calendar.tuesday().getNumber()
-              + calendar.wednesday().getNumber() + calendar.thursday().getNumber()
-              + calendar.friday().getNumber() + calendar.saturday().getNumber()
-              + calendar.sunday().getNumber())
-          < 1) {
+      if (calendar.monday().getNumber() == NOT_AVAILBLE
+          && calendar.tuesday().getNumber() == NOT_AVAILBLE
+          && calendar.wednesday().getNumber() == NOT_AVAILBLE
+          && calendar.thursday().getNumber() == NOT_AVAILBLE
+          && calendar.friday().getNumber() == NOT_AVAILBLE
+          && calendar.saturday().getNumber() == NOT_AVAILBLE
+          && calendar.sunday().getNumber() == NOT_AVAILBLE) {
         noticeContainer.addNotice(new CalendarHasNoActiveDaysNotice(
             /* serviceId= */ calendar.serviceId(),
             /* calendarCsvRowNumber= */ calendar.csvRowNumber()));
